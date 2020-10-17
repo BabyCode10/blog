03bd21bedcec3b5b9d13a99b55b8ef9daf62ec12
irfan hadian <irfan.hadian10@gmail.com>
2020-10-17 12:50:06 +0700

    updated model post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'created_at', 'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment', 'post_id');
+    }
 }
