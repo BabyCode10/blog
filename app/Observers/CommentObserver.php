0b61f1408c65a0728a6ecbc0e85df9830e76c096
irfan hadian <irfan.hadian10@gmail.com>
2020-10-19 15:19:19 +0700

    added comment observer

---
+++ app/Observers/CommentObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use Illuminate\Support\Facades\Mail;
+use App\Models\Comment;
+use App\Mail\CommentMail;
+
+
+class CommentObserver
+{
+    /**
+     * Handle the comment "created" event.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return void
+     */
+    public function created(Comment $comment)
+    {
+        Mail::to($comment->post->user->email)->send(new CommentMail($comment));
+    }
+
+    /**
+     * Handle the comment "updated" event.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return void
+     */
+    public function updated(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Handle the comment "deleted" event.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return void
+     */
+    public function deleted(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Handle the comment "restored" event.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return void
+     */
+    public function restored(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Handle the comment "force deleted" event.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return void
+     */
+    public function forceDeleted(Comment $comment)
+    {
+        //
+    }
+}
