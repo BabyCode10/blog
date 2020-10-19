14e794069674e090f35c3ff14a914e7e10aa4663
irfan hadian <irfan.hadian10@gmail.com>
2020-10-19 15:17:25 +0700

    added comment list controller

---
+++ app/Http/Controllers/CommentListController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Comment;
+
+class CommentListController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comments = Comment::with('post.user')->get();
+
+        return view('admin.comment-list.index', compact('comments'));
+    }
+}
